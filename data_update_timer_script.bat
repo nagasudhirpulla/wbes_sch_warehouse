c4ae6f28867145aca86709975da5f6a96e17282d
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-11-04 14:10:34 +0530

    changes

---
+++ data_update_timer_script.bat
@@ -0,0 +1,2 @@
+call C:\ProgramData\Anaconda3\Scripts\activate base
+call C:\ProgramData\Anaconda3\python.exe data_update_timer_script.py
--- index.py
+++ index.py
@@ -9,7 +9,7 @@ import datetime as dt
 from data_update_script import updateSchedules
 
 nowDay = dt.datetime.now()
-startDay = dt.datetime(2019, 10, 17)
+startDay = dt.datetime(2019, 11, 4)
 targetDates = []
 for dayIter in range((nowDay-startDay).days+1):
     targetDt = startDay + dt.timedelta(days=dayIter)
